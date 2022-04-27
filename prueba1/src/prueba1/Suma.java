3fd256c3cc0f82e885186215a221539ce79f5322
usuario3nombre <fanni300797@gmail.com>
2022-04-27 08:58:30 +0200

    primer commit

---
+++ prueba1/src/prueba1/Suma.java
@@ -0,0 +1,24 @@
+package prueba1;
+
+import java.util.Scanner;
+
+public class Suma {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num1, num2, suma;
+		
+		System.out.println("Introduzca un numero");
+		num1 = sc.nextInt();
+		
+		System.out.println("Introduzca otro numero");
+		num2 = sc.nextInt();
+		
+		suma = num1 + num2;
+		
+		System.out.println("La suma es: " + suma);
+		
+	}
+
+}
